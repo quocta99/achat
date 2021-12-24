1ae18ecf777ad633fe6b06ac745114dec34736be
Quốc Bình <tranaiquoc4499@gmail.com>
2021-12-25 04:00:16 +0700

    first commit

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -103,6 +103,33 @@
                                                 <span class="mb-0 text-secondary d-inline-block text-truncate">lawrece@gmail.com</span>
                                             </div>
                                         </div>
+                                        <div class="side-bar__item d-flex align-items-center p-2">
+                                            <div class="avatar">
+                                                <img width="45" height="45" src="https://hinhnen123.com/wp-content/uploads/2021/06/hinh-anh-avatar-dep-nhat-6.jpg" class="img-fluid header-avatar mr-2" alt="" />
+                                            </div>
+                                            <div class="header-box flex-fill side-bar__item-content">
+                                                <p class="header-name mb-0 d-inline-block text-truncate">Lawrence Collins</p>
+                                                <span class="mb-0 text-secondary d-inline-block text-truncate">lawrece@gmail.com</span>
+                                            </div>
+                                        </div>
+                                        <div class="side-bar__item d-flex align-items-center p-2">
+                                            <div class="avatar">
+                                                <img width="45" height="45" src="https://hinhnen123.com/wp-content/uploads/2021/06/hinh-anh-avatar-dep-nhat-6.jpg" class="img-fluid header-avatar mr-2" alt="" />
+                                            </div>
+                                            <div class="header-box flex-fill side-bar__item-content">
+                                                <p class="header-name mb-0 d-inline-block text-truncate">Lawrence Collins</p>
+                                                <span class="mb-0 text-secondary d-inline-block text-truncate">lawrece@gmail.com</span>
+                                            </div>
+                                        </div>
+                                        <div class="side-bar__item d-flex align-items-center p-2">
+                                            <div class="avatar">
+                                                <img width="45" height="45" src="https://hinhnen123.com/wp-content/uploads/2021/06/hinh-anh-avatar-dep-nhat-6.jpg" class="img-fluid header-avatar mr-2" alt="" />
+                                            </div>
+                                            <div class="header-box flex-fill side-bar__item-content">
+                                                <p class="header-name mb-0 d-inline-block text-truncate">Lawrence Collins</p>
+                                                <span class="mb-0 text-secondary d-inline-block text-truncate">lawrece@gmail.com</span>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
